16f823e9efe2076170c84f7ce4d7c79f9b879d2f
Uri Shamay <cmpxchg16@gmail.com>
2021-04-08 09:20:16 +0300

    Bump to 1.3.19

--- spectral.rb
+++ spectral.rb
@@ -1,9 +1,9 @@
 class Spectral < Formula
   desc "Automated code security for modern teams"
   homepage "https://spectralops.io"
-  url "https://get.spectralops.io/latest/dl/mac?v=1.3.7"
-  version "1.3.7"
-  sha256 "dbae1f2bf178f9e230362ee1082420b543e03858d5b426c153b60577debe650a"
+  url "https://get.spectralops.io/latest/dl/mac?v=1.3.19"
+  version "1.3.19"
+  sha256 "0009c4e3bbf6f6cc08cf346506d0b05cb7efa11d7eca3125e7a9577fca356489"
 
   def install
     bin.install "spectral"
