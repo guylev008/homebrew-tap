916a2960be1aa402e880c995194a4eea0cca05d9
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-08 17:40:12 +0300

    Brew formula update for preflight version v1.1.3

--- preflight.rb
+++ preflight.rb
@@ -5,25 +5,25 @@
 class Preflight < Formula
   desc "Verify your curl scripts and executables before running them"
   homepage "https://github.com/spectralops/preflight"
-  version "1.1.2"
+  version "1.1.3"
   license "Apache 2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.2/preflight_1.1.2_Darwin_x86_64.tar.gz"
-    sha256 "ed909081d7f745917b828898bef353d30d32b071237d5b64243fd4ce3ef9988c"
+    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.3/preflight_1.1.3_Darwin_x86_64.tar.gz"
+    sha256 "07578caac2b5aafb606356291197de542d9b7305d8c2c13681261fd74a27c522"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.2/preflight_1.1.2_Darwin_arm64.tar.gz"
-    sha256 "076e19dd4998ce2dcd71a70599f58be872efc4c0f262ad4d97f84e6fecf172c0"
+    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.3/preflight_1.1.3_Darwin_arm64.tar.gz"
+    sha256 "e07089a1f860199da4097368581ec09f4111ad1996c231cc25d8058b20a8fed9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.2/preflight_1.1.2_Linux_x86_64.tar.gz"
-    sha256 "78657277d1bda5f2474ff4b04215cf1c9632bcb63c2907a688301b651a76f94a"
+    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.3/preflight_1.1.3_Linux_x86_64.tar.gz"
+    sha256 "a2b904f3c2940a30ff6906cd8b63f59a645896d62a6c142f9bf3d81c1d354abf"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.2/preflight_1.1.2_Linux_arm64.tar.gz"
-    sha256 "061c74ea127daf2e101915e61d1b2cf1899e7236315de38f3d1d011c4a64ac8a"
+    url "https://github.com/SpectralOps/preflight/releases/download/v1.1.3/preflight_1.1.3_Linux_arm64.tar.gz"
+    sha256 "0c29c608233d4ff84f5ab782766317272e6762836ed80edddc2698e4793474bb"
   end
 
   def install
