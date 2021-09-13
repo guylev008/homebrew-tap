8ff7d7c304b484d5091f652c6185cc68f631e7b0
Uri Shamay <cmpxchg16@gmail.com>
2021-09-13 17:29:34 +0300

    Update spectral.rb

--- spectral.rb
+++ spectral.rb
@@ -1,9 +1,9 @@
 class Spectral < Formula
   desc "Automated code security for modern teams"
   homepage "https://spectralops.io"
-  url "https://get.spectralops.io/latest/dl/mac?v=1.4.1"
-  version "1.4.1"
-  sha256 "a51f85eb8cf1bda36f070243b5024355d23221a459e645bde8ef42f3532b5220"
+  url "https://get.spectralops.io/latest/dl/mac?v=1.7.0"
+  version "1.7.0"
+  sha256 "0fe0a98c4dbbcd2965c3b66eeabefc110616a2462d5469101757696113820cdb"
 
   def install
     bin.install "spectral"
