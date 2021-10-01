bc173f4a9324dafc88b8af132ccead510f315532
Dotan J. Nahum <jondotan@gmail.com>
2021-10-01 15:49:54 +0300

    Update keyscope.rb

--- keyscope.rb
+++ keyscope.rb
@@ -2,7 +2,7 @@ class Keyscope < Formula
   desc "Keyscope is a key and secret workflow (validation, invalidation, etc.) tool built in Rust"
   homepage "https://github.com/SpectralOps/keyscope"
   url "-----"
-  version "-----"
+  version "0.0.0"
   sha256 "-----"
 
   def install
