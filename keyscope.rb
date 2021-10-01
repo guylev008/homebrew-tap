bdaaa6d7ff3a6733243ac4334a33906c7eeab87c
Dotan J. Nahum <jondotan@gmail.com>
2021-10-01 15:05:34 +0300

    Create keyscope.rb

---
+++ keyscope.rb
@@ -0,0 +1,11 @@
+class Keyscope < Formula
+  desc "Keyscope is a key and secret workflow (validation, invalidation, etc.) tool built in Rust"
+  homepage "https://github.com/SpectralOps/keyscope"
+  url "-----"
+  version "-----"
+  sha256 "-----"
+
+  def install
+    bin.install "keyscope"
+  end
+end
