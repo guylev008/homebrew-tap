a7f85038abc1753129eed1c20305ee803977da6e
Dotan J. Nahum <jondotan@gmail.com>
2021-10-23 18:49:54 +0300

    Update teller.rb

--- teller.rb
+++ teller.rb
@@ -7,7 +7,6 @@ class Teller < Formula
   homepage "https://github.com/spectralops/teller"
   version "1.4.0"
   license "Apache 2.0"
-  bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/SpectralOps/teller/releases/download/v1.4.0/teller_1.4.0_Darwin_x86_64.tar.gz"
