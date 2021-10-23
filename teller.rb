f4306797e9a69731192d829d1cf348a878254a9a
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-23 18:49:13 +0300

    Brew formula update for teller version v1.4.0

--- teller.rb
+++ teller.rb
@@ -5,24 +5,25 @@
 class Teller < Formula
   desc "A secret manager for developers - never leave your terminal for secrets"
   homepage "https://github.com/spectralops/teller"
-  version "1.3.0"
+  version "1.4.0"
   license "Apache 2.0"
+  bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/SpectralOps/teller/releases/download/v1.3.0/teller_1.3.0_Darwin_x86_64.tar.gz"
-    sha256 "28e909433ba15463fd5aa89695a06f15ac64e98f9f3df4ddeaeb187d1c029523"
+    url "https://github.com/SpectralOps/teller/releases/download/v1.4.0/teller_1.4.0_Darwin_x86_64.tar.gz"
+    sha256 "80a71bd3b60d207ce30c4ec633725b113ad134601b8edbddc643349723099238"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/SpectralOps/teller/releases/download/v1.3.0/teller_1.3.0_Darwin_arm64.tar.gz"
-    sha256 "41e7337ee000eb55cbc7c075e3468fe19b8d4310382042dff318db0c79e3d052"
+    url "https://github.com/SpectralOps/teller/releases/download/v1.4.0/teller_1.4.0_Darwin_arm64.tar.gz"
+    sha256 "7ab41f8b9f9a55b493ec50cc962793b54fe5b1ebcb2bf49737826de7d16c4f7d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/SpectralOps/teller/releases/download/v1.3.0/teller_1.3.0_Linux_x86_64.tar.gz"
-    sha256 "fab24fa108b96dc9228b12dd7f4148561752afa785c19d871bfee08a430a771e"
+    url "https://github.com/SpectralOps/teller/releases/download/v1.4.0/teller_1.4.0_Linux_x86_64.tar.gz"
+    sha256 "3429ee9363fc822018c81cc56172470947177d6c22d74945cbd2931e1005ae42"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/SpectralOps/teller/releases/download/v1.3.0/teller_1.3.0_Linux_arm64.tar.gz"
-    sha256 "0fc40b662c6c063831e1214a6e07f7c25b4ee6457b1369f6af341188bedd17dc"
+    url "https://github.com/SpectralOps/teller/releases/download/v1.4.0/teller_1.4.0_Linux_arm64.tar.gz"
+    sha256 "00e3097fd4791556c3ae7c57e6dd4cea79e837621f5ebdb47048a5723b881efb"
   end
 
   def install
