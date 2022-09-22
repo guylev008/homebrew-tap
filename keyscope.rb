85c00ba64f6a6872e273c6a338474919fbd1452c
Dotan J. Nahum <jondotan@gmail.com>
2022-09-22 21:28:04 +0300

    updating formula for keyscope

--- keyscope.rb
+++ keyscope.rb
@@ -1,9 +1,9 @@
 class Keyscope < Formula
   desc "Keyscope is a key and secret workflow (validation, invalidation, etc.) tool built in Rust"
   homepage "https://github.com/SpectralOps/keyscope"
-  url "https://github.com/spectralops/keyscope/releases/download/v1.2.2/keyscope-v1.2.2-x86_64-macos.tar.xz"
-  version "1.2.2"
-  sha256 "9929b27400366784224c73aee5e6b205a171a08bd1afab3e3e15050bed3c195e"
+  url "https://github.com/spectralops/keyscope/releases/download/v1.2.3/keyscope-v1.2.3-x86_64-macos.tar.xz"
+  version "1.2.3"
+  sha256 "b502999be19879f9d08499ed5c4c986f7cbc0952ac0f6f700a46d3b37d87f026"
 
   def install
     bin.install "keyscope"
