fb2401cbc53818e4f1920f38135df29c205ac304
Dotan J. Nahum <jondotan@gmail.com>
2023-03-10 10:12:17 +0200

    rustwrap update: keyscope.rb

--- keyscope.rb
+++ keyscope.rb
@@ -1,9 +1,9 @@
 class Keyscope < Formula
   desc "Keyscope is a key and secret workflow (validation, invalidation, etc.) tool built in Rust"
-  homepage "https://github.com/SpectralOps/keyscope"
-  url "https://github.com/spectralops/keyscope/releases/download/v1.2.3/keyscope-v1.2.3-x86_64-macos.tar.xz"
-  version "1.2.3"
-  sha256 "b502999be19879f9d08499ed5c4c986f7cbc0952ac0f6f700a46d3b37d87f026"
+  homepage "http://github.com/spectralops/keyscope"
+  url "https://github.com/spectralops/keyscope/releases/download/v1.3.0/keyscope-x86_64-macos.tar.xz"
+  version "1.3.0"
+  sha256 "8898d14e1ebbb3571a99dcc78903b6a838eba0e166bf2f2b5fc9ce84c75959bb"
 
   def install
     bin.install "keyscope"
